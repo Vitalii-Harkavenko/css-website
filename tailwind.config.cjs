3d13d6c371e9379aa30bfe1172bb4d4e2a6791e6
Vitalii-Harkavenko <vitinfo22@gmail.com>
2023-03-19 00:33:22 +0100

    adding fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,16 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'bidenatrial': ['Bidenatrial', 'sans-serif'],
+        'des-morgan': ['Des-Morgan', 'sans-serif'],
+        'glamore': ['Glamore', 'sans-serif'],
+        'madyntha-geradine': ['Madyntha-Geradine', 'sans-serif'],
+        'nolluqa': ['Nolluqa', 'sans-serif'],
+        'redemption': ['Redemption', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
