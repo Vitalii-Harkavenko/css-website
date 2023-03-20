4c982247bacb2968d9971ddfecb451f2f052b51b
Vitalii-Harkavenko <vitinfo22@gmail.com>
2023-03-20 17:29:42 +0100

    finished home navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,16 @@
 import HomeNavbar from "./HomeNavbar"
 export default function Home() {
 	return (
-		<div className="content bg-neutral-900">
+		<div className="content bg-gradient-to-r from-neutral-900 to bg-neutral-800 gap-8 flex flex-col">
 			<HomeNavbar />
+			<main className="text-2xl text-white my-[5vw]">
+				<div className="desc-shadow">
+					<p className="desc h-40 bg-neutral-900">
+						So this is CSS oriented portfolio website about some tech products and since the goal
+						is to showcase my design abilities don't expect anything realistic here :D 
+					</p>
+				</div>
+			</main>
 		</div>
 	)
 }
--- src/components/HomeNavbar.jsx
+++ src/components/HomeNavbar.jsx
@@ -2,23 +2,25 @@ import { Link } from "react-router-dom"
 
 export default function HomeNavbar() {
 	return (
-		<nav className="home-navbar bg-yellow-500 h-64">
-			<div className="home-navbar-shadow">
-				<ul className="home-navbar-line bg-yellow-400 h-48 grid grid-cols-3 text-5xl leading-normal">
-					<li className="h-16 mt-8 relative group">
-						<Link to="/" className="font-glamore animation-nav-main">HOME</Link>
-						<Link to="/" className="font-nolluqa animation-nav-hovered whitespace-nowrap">YOU'Re AlReADY THeRe</Link>
-					</li>
-					<li className="h-16 my-auto relative group">
-						<Link to="/products" className="font-des-morgan animation-nav-main">PRODUCTS</Link>
-						<Link to="/products" className="font-chaviera animation-nav-hovered">PRODUCTS</Link>
-					</li>
-					<li className="h-16 mb-8 mt-auto relative group">
-						<Link to="/about" className="font-madyntha-geradine animation-nav-main">About</Link>
-						<Link to="/about" className="font-redemption animation-nav-hovered">About</Link>
-					</li>
-				</ul>
-			</div>
-		</nav>
+		<div className="drop-shadow-2xl">
+			<nav className="home-navbar bg-gradient-to-r from-yellow-500 to-amber-600 h-64">
+				<div className="home-navbar-shadow">
+					<ul className="home-navbar-line bg-gradient-to-l from-yellow-400 to-amber-400 h-48 grid grid-cols-3 text-5xl leading-normal">
+						<li className="h-16 mt-8 relative group">
+							<Link to="/" className="font-glamore animation-nav-main">HOME</Link>
+							<Link to="/" className="font-nolluqa animation-nav-hovered whitespace-nowrap">YOU'Re AlReADY THeRe</Link>
+						</li>
+						<li className="h-16 my-auto relative group">
+							<Link to="/products" className="font-des-morgan animation-nav-main">PRODUCTS</Link>
+							<Link to="/products" className="font-chaviera animation-nav-hovered">PRODUCTS</Link>
+						</li>
+						<li className="h-16 mb-8 mt-auto relative group">
+							<Link to="/about" className="font-madyntha-geradine animation-nav-main">About</Link>
+							<Link to="/about" className="font-redemption animation-nav-hovered">About</Link>
+						</li>
+					</ul>
+				</div>
+			</nav>
+		</div>
 	)
 }
